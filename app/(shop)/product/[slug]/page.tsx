61d4b60a6e1644b9dd0bd7088a8ef8dec1eff1b3
Adeolu Amole <amoleadeoluadex@gmail.com>
2026-04-24 22:22:44 +0100

    feat: New Updates

--- app/(shop)/product/[slug]/page.tsx
+++ app/(shop)/product/[slug]/page.tsx
@@ -3,6 +3,7 @@
 import { getProductBySlug } from '@/app/services/product-service'
 import { notFound } from 'next/navigation'
 import ProductDetailsClient from '@/app/components/shop/ProductDetailsClient'
+import ProductHydrator from '@/app/components/shop/ProductHydrator'
 
 interface Props {
   params: Promise<{ slug: string }>
@@ -10,17 +11,21 @@ interface Props {
 
 export default async function ProductPage({ params }: Props) {
   const { slug } = await params
-
   const product = await getProductBySlug(slug)
-
-  console.log('Fetched product for slug:', slug, product)
 
   if (!product) {
     notFound()
   }
 
-  // Serialize MongoDB data
   const serializedProduct = JSON.parse(JSON.stringify(product))
 
-  return <ProductDetailsClient product={serializedProduct} />
+  return (
+    <>
+      {/* 1. Sync this specific product to the Zustand store */}
+      <ProductHydrator products={[serializedProduct]} />
+
+      {/* 2. Render the UI */}
+      <ProductDetailsClient product={serializedProduct} />
+    </>
+  )
 }
