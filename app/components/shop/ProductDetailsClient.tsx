5b9d5499d51c777d3c88bcdb70be8451ce2ad314
Adeolu Amole <amoleadeoluadex@gmail.com>
2026-04-29 12:51:35 +0100

    feat: New Updates

--- app/components/shop/ProductDetailsClient.tsx
+++ app/components/shop/ProductDetailsClient.tsx
@@ -279,7 +279,7 @@ export default function ProductDetailsClient({
               key={index}
               c={index === breadcrumbs.length - 1 ? 'dimmed' : 'blue'}
               underline="hover"
-              className='text-sm!'
+              className='text-xs! text-sm!'
             >
               {item.title}
             </Anchor>
