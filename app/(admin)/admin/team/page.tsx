85fd17dcdd5dcc95e26a8c1f921c0350ac571227
Adeolu Amole <amoleadeoluadex@gmail.com>
2026-04-30 15:07:03 +0100

    feat: New Updates

--- app/(admin)/admin/team/page.tsx
+++ app/(admin)/admin/team/page.tsx
@@ -2,6 +2,7 @@
 import { getAdminStaff } from '@/app/services/adminService'
 import TeamClient from '@/app/components/admin/team/TeamClient'
 
+export const dynamic = 'force-dynamic'
 export default async function TeamPage() {
   const initialStaff = await getAdminStaff()
 
